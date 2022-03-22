b32d16662c6b225bfac0546f0f3062fd02e43316
Tuan Luu <tuan.luu@asnet.com.vn>
2022-03-22 23:47:09 +0700

    Update render markdown as html string

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,6 +11,8 @@ const Post = ({
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }}/>
     </Layout>
   )
 }
@@ -30,7 +32,7 @@ export const getStaticProps = async ({
   params
 }) => {
   // Fetch necessary data for the blog post using params.id
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
