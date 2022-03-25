d96628634cd2dcc5269de9183bab73520733ef82
Tuan Luu <tuan.luu@asnet.com.vn>
2022-03-25 21:13:47 +0700

    Fix error getLayout on Component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,22 @@
 import { AppProps } from 'next/app'
+import { ReactNode } from 'react';
+import { NextPage } from "next";
 import '../styles/global.css'
+
+// Fix error getLayout: https://dev.to/ofilipowicz/next-js-per-page-layouts-and-typescript-lh5
+type Page<P = {}> = NextPage<P> & {
+  getLayout?: (page: ReactNode) => ReactNode;
+};
+
+type Props = AppProps & {
+  Component: Page;
+};
 
 const App = ({
   Component,
   pageProps
-}: AppProps) => {
-  const getLayout = Component.getLayout || ((page) => page)
+}: Props) => {
+  const getLayout = Component.getLayout || ((page: ReactNode) => page)
   return getLayout(<Component {...pageProps} />)
 }
 
